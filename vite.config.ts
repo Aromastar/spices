19402be3d17d7198c250b7f209362dfc2b48a576
Aleksey Solovyev <solovyev.a@icloud.com>
2024-11-16 10:13:07 +0300

    feat: add njk support

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
+import nunjucksPlugin from 'vite-plugin-nunjucks'
 
 const SRC_DIR = resolve(__dirname, 'src')
 
 export default defineConfig({
   build: { outDir: resolve(__dirname, 'dist') },
   css: { preprocessorOptions: { sass: { api: 'modern' } } },
+  plugins: [nunjucksPlugin()],
   resolve: {
     alias: {
       '@styles': resolve(SRC_DIR, 'styles')
