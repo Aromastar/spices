18ecec0030fde69c97f80a26b2b53a5c510c4692
Aleksey Solovyev <solovyev.a@icloud.com>
2024-11-16 10:02:55 +0300

    feat: add styles folder alias

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,10 @@ const SRC_DIR = resolve(__dirname, 'src')
 
 export default defineConfig({
   build: { outDir: resolve(__dirname, 'dist') },
+  resolve: {
+    alias: {
+      '@styles': resolve(SRC_DIR, 'styles')
+    }
+  },
   root: SRC_DIR
 })
