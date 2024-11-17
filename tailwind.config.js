5ed9bfcc70736724ff379101db3b9be683718350
Aleksey Solovyev <solovyev.a@icloud.com>
2024-11-17 13:10:02 +0300

    feat: change default tailwind container styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,12 @@ export default {
   content: ['./src/**/*.{js,ts,jsx,tsx,html}'],
   plugins: [],
   theme: {
-    extend: {},
-  },
+    extend: {
+      container: {
+        center: true,
+        padding: '2%',
+        screens: {}
+      }
+    }
+  }
 }
-
